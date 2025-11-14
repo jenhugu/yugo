5b8c339d83efaa280af438d5e0deb4316ed77f40
vincent <vincent.horo@outlook.fr>
2025-11-14 19:59:30 +0100

    conflict resolution

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  # Defines the root path route ("/")
   root to: 'pages#home'
   resources :trips, only: %i[index show new create edit update] do
     member do
@@ -14,6 +15,4 @@ Rails.application.routes.draw do
     end
     resources :itineraries, only: %i[show]
   end
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
