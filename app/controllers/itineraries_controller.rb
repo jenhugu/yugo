491c00c95f14222c2b5e8077d9fb2f8f688023d5
vincent <vincent.horo@outlook.fr>
2025-11-15 09:50:26 +0100

    activity_card being created

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -3,4 +3,11 @@ class ItinerariesController < ApplicationController
     # @trip = Trip.find(params[:trip_id])
     # @itinerary = @trip.itineraries.find(params[:id])
   end
+
+  def create
+    # LLM prompt pour creer l'itineraire en allant chercher les actvitiés
+    # sa sera vie le LLM prompt que les dates etc... de Itinerary_item
+    # seront cree
+    # on ira chercher les activités via la recommendation
+  end
 end
