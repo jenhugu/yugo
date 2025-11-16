de1e98ac522a568950ada9c59e7b38c71b4398f2
vincent <vincent.horo@outlook.fr>
2025-11-16 10:40:58 +0100

    activity_card done

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,10 +1,11 @@
 class ItinerariesController < ApplicationController
   def show
-    # @trip = Trip.find(params[:trip_id])
+    @trip = Trip.find(params[:trip_id])
     # @itinerary = @trip.itineraries.find(params[:id])
   end
 
   def create
+    # Déclencher quand la recommendation est validée
     # LLM prompt pour creer l'itineraire en allant chercher les actvitiés
     # sa sera vie le LLM prompt que les dates etc... de Itinerary_item
     # seront cree
