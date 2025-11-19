511422b156570c1fbec6555e06f365691728103a
vincent <vincent.horo@outlook.fr>
2025-11-19 17:44:38 +0100

    trying

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -7,12 +7,11 @@ class ItinerariesController < ApplicationController
   def create
     # Déclencher quand la recommendation est validée
     # idee : if recommendation_reviewed = true && recommendation = accepted then LLM
-    @itinerary = Itinerary.new(recommendation_params)
 
-    # LLM prompt pour creer l'itineraire en allant chercher les actvitiés
-    # sa sera vie le LLM prompt que les dates etc... de Itinerary_item
-    # seront cree
-    # on ira chercher les activités via la recommendation
+    # on fais un nouveau RubyLLM.chat.ask("prompt_LLM").content
+    # on itére sur chaque activitée de la recommendation approuvée pour leurs mettre une date slot etc ...
+    # qu'on enregistre dans Itinerary_item
+
     # dans un deuxième temps on passera sa en background job
   end
 end
