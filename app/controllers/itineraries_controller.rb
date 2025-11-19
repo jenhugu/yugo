f38fff606605a73b138813f8d3adf3552a006202
vincent <vincent.horo@outlook.fr>
2025-11-19 17:12:38 +0100

    change itinerary_item date format

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -6,9 +6,13 @@ class ItinerariesController < ApplicationController
 
   def create
     # Déclencher quand la recommendation est validée
+    # idee : if recommendation_reviewed = true && recommendation = accepted then LLM
+    @itinerary = Itinerary.new(recommendation_params)
+
     # LLM prompt pour creer l'itineraire en allant chercher les actvitiés
     # sa sera vie le LLM prompt que les dates etc... de Itinerary_item
     # seront cree
     # on ira chercher les activités via la recommendation
+    # dans un deuxième temps on passera sa en background job
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_18_173641) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_19_155845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_18_173641) do
   end
 
   create_table "itinerary_items", force: :cascade do |t|
-    t.string "date"
+    t.date "date"
     t.string "slot"
     t.string "time"
     t.string "position"
