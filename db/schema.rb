a6fa55e8cecc0ceb43e455a553293a1a89c7326e
vincent <vincent.horo@outlook.fr>
2025-11-19 17:31:09 +0100

    itinerary_item DB updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_19_155845) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_19_161312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,8 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_19_155845) do
   create_table "itinerary_items", force: :cascade do |t|
     t.date "date"
     t.string "slot"
-    t.string "time"
-    t.string "position"
+    t.time "time"
+    t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "itinerary_id", null: false
