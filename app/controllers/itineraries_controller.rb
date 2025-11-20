f2d299f55d6b7f5c5f3eea41b8262bd06c1d1510
vincent <vincent.horo@outlook.fr>
2025-11-20 07:53:46 +0100

    correction for presentation

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -4,18 +4,4 @@ class ItinerariesController < ApplicationController
     @trip = Trip.find(params[:trip_id])
     @itinerary = @trip.itineraries.find(params[:id])
   end
-
-  private
-
-  def verify_acceptance_recommendation
-    # verfier a chaque vote pour permettre ensuite le background job
-    # a mettre dans le controleur recommendation, si retourne false alors ne fais
-    # rien mais si retourne true alors déclenche dans dossier job la fonction LLM
-    # generate_itinerary
-    # user_trip_status = UserTripStatus.find
-    # recommendation = recommendation.find
-    if recommendation.accepted && user_trip_status.recommendation_reviewed
-      # alors déclenche generate_itinerary job
-    end
-  end
 end
