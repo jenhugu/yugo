cdaabe1c82900d57cb986b1cd611283f49c2859c
Jennifer Huguenin <huguenin.jennifer@gmail.com>
2025-11-25 21:28:36 +0100

    prevented duplicates in a recommendation

--- app/models/recommendation_item.rb
+++ app/models/recommendation_item.rb
@@ -3,4 +3,7 @@ class RecommendationItem < ApplicationRecord
   belongs_to :activity_item
   has_many :recommendation_votes, dependent: :destroy
   has_many :user_trip_statuses, through: :recommendation_votes
+
+  # Empêche qu'une même activité soit recommandée deux fois dans la même recommendation
+  validates :activity_item_id, uniqueness: { scope: :recommendation_id, message: "is already in this recommendation" }
 end
