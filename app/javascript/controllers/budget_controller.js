a158b645a862f313f57ccfaf8a4cc2d23aa3756a
Nawel Paschy <naweljpaschy@gmail.com>
2025-11-27 13:08:47 +0100

    font updated

--- app/javascript/controllers/budget_controller.js
+++ app/javascript/controllers/budget_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static values = { initial: Number }
 
   connect() {
-    this.valueTarget.textContent = `${this.initialValue}€`
+    this.valueTarget.textContent = `${this.rangeTarget.value}€`
   }
 
   update() {
