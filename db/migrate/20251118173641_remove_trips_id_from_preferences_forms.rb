a73a95d4d72747edc7a4496ff63cb8a07fe23c51
Jennifer Huguenin <huguenin.jennifer@gmail.com>
2025-11-28 14:16:41 +0100

    Make remove_trips_id migration safe with if_exists: true

--- db/migrate/20251118173641_remove_trips_id_from_preferences_forms.rb
+++ db/migrate/20251118173641_remove_trips_id_from_preferences_forms.rb
@@ -1,5 +1,5 @@
 class RemoveTripsIdFromPreferencesForms < ActiveRecord::Migration[7.1]
   def change
-    remove_column :preferences_forms, :trips_id, :bigint
+    remove_column :preferences_forms, :trips_id, :bigint, if_exists: true
   end
 end
