b1a9541ae0db87e529b11e1786dffc27ad4be86f
vincent <vincent.horo@outlook.fr>
2025-12-02 11:20:06 +0100

    6 images added for the trips

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,5 +1,11 @@
 module TripsHelper
+  PARIS_TRIP_IMAGES = (1..6).map { |i| "Paris_trip_#{i}.jpg" }.freeze
+
   def pending_invitation?(user_trip_status)
     user_trip_status.invitation_accepted == false
+  end
+
+  def random_trip_image
+    PARIS_TRIP_IMAGES.sample
   end
 end
